e3fdc1440959828663fe0cec561c058297865e0f
Leo Gordillo <leogordillo@timeitgoes.com>
2019-10-31 06:42:50 -0500

    Added Reservation Schema unit test

---
+++ test/unit/lib/reservation.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const should = chai.should();
+const Reservation = require('../../../lib/schema/reservation');
+const reservations = require('../../../lib/reservations');
+
+describe('Reservation Library', function() {
+    context('Validate', function() {
+        it('should pass a valid reservation with no optional fields', function() {
+
+        });
+    });
+});
